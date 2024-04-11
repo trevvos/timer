aa01a6ab2ec65bf4d5540054c61bb419a4bbe16a
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-04-11 09:15:33 -0300

    styles page history

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,11 @@ import {
   StartCountdownButton,
   TaskInput
 } from './styles'
+import { useState } from 'react'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <FormContainer>
@@ -18,6 +21,8 @@ export function Home() {
           id="task"
           list="task-suggestions"
           placeholder="Dê um nome para seu projeto"
+          onChange={e => setTask(e.target.value)}
+          value={task}
         />
 
         <datalist id="task-suggestions">
@@ -48,7 +53,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
