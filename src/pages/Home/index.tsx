2e2f1296d2e5e862617ec1c026eec09d62cc9f69
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-04-13 12:47:57 -0300

    vallidation with lib ZOD

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -8,56 +12,85 @@ import {
   StartCountdownButton,
   TaskInput
 } from './styles'
-import { useState } from 'react'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser no minimo 60 minutos.')
+    .max(60, 'O ciclo precisa ser no maximo 60 minutos.')
+})
+
+/*interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}*/
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const [task, setTask] = useState('')
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <FormContainer>
-        <label htmlFor="task">Vou trabalhar em</label>
-        <TaskInput
-          id="task"
-          list="task-suggestions"
-          placeholder="Dê um nome para seu projeto"
-          onChange={e => setTask(e.target.value)}
-          value={task}
-        />
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para seu projeto"
+            {...register('task')}
+          />
 
-        <datalist id="task-suggestions">
-          <option value="Projeto 1"></option>
-          <option value="Projeto 2"></option>
-          <option value="Projeto 3"></option>
-          <option value="Projeto 4"></option>
-          <option value="Projeto 5"></option>
-        </datalist>
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+            <option value="Projeto 5"></option>
+          </datalist>
 
-        <label htmlFor="minutesAmount">durante</label>
-        <MinutesAmountInput
-          type="number"
-          id="minutesAmount"
-          placeholder="00"
-          step={5}
-          min={5}
-          max={60}
-        />
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
-        <span>minutos.</span>
+          <span>minutos.</span>
 
-        <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
-          <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
-        </CountdownContainer>
+          <CountdownContainer>
+            <span>0</span>
+            <span>0</span>
+            <Separator>:</Separator>
+            <span>0</span>
+            <span>0</span>
+          </CountdownContainer>
 
-        <StartCountdownButton disabled={!task} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
-      </FormContainer>
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        </FormContainer>
+      </form>
     </HomeContainer>
   )
 }
